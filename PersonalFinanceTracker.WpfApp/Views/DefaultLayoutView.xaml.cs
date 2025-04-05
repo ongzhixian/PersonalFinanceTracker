0bd8f10e611a2bda26c07de4350ef5005aa6cd4d
ongzhixian <zhixian@hotmail.com>
2025-04-05 22:42:43 +0800

    Login screen

---
+++ PersonalFinanceTracker.WpfApp/Views/DefaultLayoutView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonalFinanceTracker.WpfApp.Views
+{
+    /// <summary>
+    /// Interaction logic for DefaultLayoutView.xaml
+    /// </summary>
+    public partial class DefaultLayoutView : UserControl
+    {
+        public DefaultLayoutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
