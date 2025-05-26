f38820781d8ce9e442b498cefd0180d03aa296df
ongzhixian <zhixian@hotmail.com>
2025-05-26 20:54:59 +0800

    add todo

---
+++ aws/todo.py
@@ -0,0 +1,91 @@
+"""
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
+pip install azure-identity
+pip install msgraph-sdk
+https://entra.microsoft.com/#home
+https://learn.microsoft.com/en-us/graph/tutorials/python?tabs=aad&tutorial-step=3
+https://github.com/microsoftgraph/msgraph-training-python
+
+"""
+
+
+import asyncio
+import configparser
+from msgraph.generated.models.o_data_errors.o_data_error import ODataError
+from graph import Graph
+
+# Temporary placeholder
+class Graph:
+    def __init__(self, config):
+        self.settings = config
+
+async def greet_user(graph: Graph):
+    # TODO
+    return
+
+async def display_access_token(graph: Graph):
+    # TODO
+    return
+
+async def list_inbox(graph: Graph):
+    # TODO
+    return
+
+async def send_mail(graph: Graph):
+    # TODO
+    return
+
+async def make_graph_call(graph: Graph):
+    # TODO
+    return
+
+async def main():
+    print('Python Graph Tutorial\n')
+
+    # Load settings
+    config = configparser.ConfigParser()
+    config.read(['config.cfg', 'config.dev.cfg'])
+    azure_settings = config['azure']
+
+    graph: Graph = Graph(azure_settings)
+
+    await greet_user(graph)
+
+    choice = -1
+
+    while choice != 0:
+        print('Please choose one of the following options:')
+        print('0. Exit')
+        print('1. Display access token')
+        print('2. List my inbox')
+        print('3. Send mail')
+        print('4. Make a Graph call')
+
+        try:
+            choice = int(input())
+        except ValueError:
+            choice = -1
+
+        try:
+            if choice == 0:
+                print('Goodbye...')
+            elif choice == 1:
+                await display_access_token(graph)
+            elif choice == 2:
+                await list_inbox(graph)
+            elif choice == 3:
+                await send_mail(graph)
+            elif choice == 4:
+                await make_graph_call(graph)
+            else:
+                print('Invalid choice!\n')
+        except ODataError as odata_error:
+            print('Error:')
+            if odata_error.error:
+                print(odata_error.error.code, odata_error.error.message)
+
+
+
+if __name__ == '__main__':
+    # Run main
+    asyncio.run(main())
