898a8a88af5fd68aef1388b79823b707a945931b
ongzhixian <zhixian@hotmail.com>
2025-06-24 21:13:41 +0800

    update register-user-credential-form.js

--- aws/s3/emptool-public/user-credential/components/register-user-credential-form.js
+++ aws/s3/emptool-public/user-credential/components/register-user-credential-form.js
@@ -1,19 +1,29 @@
+import UserCredentialModule from './user-credential-module.js';
+
 class RegisterUserCredentialForm extends HTMLElement {
-    AUTH_TICKET_STORAGE_KEY = "test_auth_ticket";
 
     constructor() {
-        super(); // Always call super first in constructor
+        super();
         this.onRegisterButtonClicked = (e) => this.#onRegisterButtonClicked(e);
-        this.authenticator = new AuthenticationModule();
+        this.onRegisterAnotherButtonClicked = (e) => this.#onRegisterAnotherButtonClicked(e);
+        this.userCredentialModule = new UserCredentialModule();
         this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
         this.render();
+        this.sectionFeedback = this.shadowRoot.querySelector('section.feedback');
+        this.loginForm = this.shadowRoot.querySelector('form#loginForm');
+
+        this.errorMessage = this.shadowRoot.querySelector('div#errorMessage');
         this.usernameInput = this.shadowRoot.querySelector('#usernameInput');
         this.passwordInput = this.shadowRoot.querySelector('#passwordInput');
+        this.passwordConfirmInput = this.shadowRoot.querySelector('#passwordConfirmInput');
         this.registerButton = this.shadowRoot.querySelector('#registerButton');
         this.registerButton.addEventListener('click', this.onRegisterButtonClicked);
+
+        this.registerAnotherButton = this.shadowRoot.querySelector('#registerAnotherButton');
+        this.registerAnotherButton.addEventListener('click', this.onRegisterAnotherButtonClicked);
     }
 
     disconnectedCallback() {
@@ -23,21 +33,40 @@ class RegisterUserCredentialForm extends HTMLElement {
     async #onRegisterButtonClicked(e) {
         console.debug("Register button clicked.");
 
-        // let isValidCredential = await this.authenticator.validateCredentials(this.usernameInput.value, this.passwordInput.value);
+        // Validate the form inputs
+        if (this.usernameInput.value === '' || this.passwordInput.value === '' || this.passwordConfirmInput.value === '') {
 
-        // if (isValidCredential) {
+        }
 
-        //     const params = new URLSearchParams(window.location.search);
-        //     if (params.has('redirect')) {
-        //         window.location.href = params.get('redirect')
-        //     } else {
-        //         window.location.href = '/index.html'; // Redirect to home page
-        //     }
-        //     return;
-        // }
+        let registrationResponse = await this.userCredentialModule.registerUserCredential(this.usernameInput.value, this.passwordInput.value);
+        console.log('registrationResponse', registrationResponse);
 
-        //console.error("Validation failed. Please check your credentials.");
+        // {is_success: true, message: 'testuser5 added successfully', data_object: null}
+        if (registrationResponse.is_success) {
+            console.log("Registration successful:", registrationResponse.message);
+            // Hide form and show feedback
+            //this.sectionFeedback.getElementsByTagName('p')[0].innerHTML = registrationResponse.message;
+            this.loginForm.classList.add('hide');
+            this.sectionFeedback.classList.remove('hide');
+        } else {
+            // Update the error messaage and show feedback
+            this.errorMessage.innerHTML = `<span>${registrationResponse.message}</span>`;
+            this.errorMessage.classList.remove('hide');
+        }
+    }
 
+    async #onRegisterAnotherButtonClicked(e) {
+        console.debug("Register another button clicked.");
+
+        // Clear the form inputs
+        this.usernameInput.value = '';
+        this.passwordInput.value = '';
+        this.passwordConfirmInput.value = '';
+
+        // Hide feedback section and show the form again
+        this.errorMessage.classList.add('hide');
+        this.sectionFeedback.classList.add('hide');
+        this.loginForm.classList.remove('hide');
     }
 
     render() {
@@ -60,24 +89,42 @@ class RegisterUserCredentialForm extends HTMLElement {
     margin-right: .5rem;
     margin-bottom: 1.5rem;
 }
+.error {
+    color: var(--color8);
+}
+.tooltips span {
+    cursor: help;
+}
+.tooltips span:hover {
+    color: var(--color9);
+}
 </style>
+<section class="feedback hide">
+    <p>Added successfully</p>
+
+    <div>
+        <input class="button-primary" type="button" value="Register another" id="registerAnotherButton" />
+    </div>
+</section>
 <form id="loginForm">
     <label for="usernameInput">Username</label>
-    <input class="u-full-width" type="text" placeholder="Username (email)" id="usernameInput" value="testuser1" />
-    <div></div>
+    <input class="u-full-width" type="text" placeholder="Username (email)" id="usernameInput" value="testuser5" />
+    <div class='tooltips'><span title='Required'>&sharp;1</span> <span title='Existing'>&sharp;2</span></div>
 
     <label for="passwordInput">Password</label>
-    <input class="u-full-width" type="password" placeholder="Password" id="passwordInput" value="testuser1a" />
-    <div></div>
+    <input class="u-full-width" type="password" placeholder="Password" id="passwordInput" value="testuser5a" />
+    <div class='tooltips'><span title='Mismatching'>&sharp;3</span></div>
 
-    <label for="passwordInput">Password<br/>(confirm)</label>
-    <input class="u-full-width" type="password" placeholder="Password" id="passwordInput" value="testuser1a" />
-    <div></div>
+    <label for="passwordConfirmInput">Password<br/>(confirm)</label>
+    <input class="u-full-width" type="password" placeholder="Password" id="passwordConfirmInput" value="testuser5a" />
+    <div class='tooltips'><span title='Mismatching'>&sharp;3</span></div>
 
     <div></div>
-    <input class="button-primary" type="button" value="Register" id="registerButton" />
+    <div>
+        <input class="button-primary" type="button" value="Register" id="registerButton" />
+        <div class="error message hide" id="errorMessage"><div>
+    </div>
     <div></div>
-    
 </form>
 `;
     }
