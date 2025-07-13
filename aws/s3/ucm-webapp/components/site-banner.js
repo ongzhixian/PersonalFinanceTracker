5012350a6e00e9734ab159ae57225f41da895592
ongzhixian <zhixian@hotmail.com>
2025-07-13 10:58:14 +0800

    UCM-7 Add test webapp files

---
+++ aws/s3/ucm-webapp/components/site-banner.js
@@ -0,0 +1,44 @@
+class SiteBanner extends HTMLElement {
+    //static observedAttributes = ["color", "size"];
+
+    constructor() {
+        super(); // Always call super first in constructor
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        //console.log("SiteBanner added to page.");
+        this.render();
+    }
+
+    disconnectedCallback() {
+        //console.log("SiteBanner removed from page.");
+    }
+
+    connectedMoveCallback() {
+        //console.log("SiteBanner moved with moveBefore()");
+    }
+
+    adoptedCallback() {
+        //console.log("SiteBanner moved to new page.");
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        //console.log(`Attribute ${name} has changed.`);
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+<style>
+    :host {}
+    h1 {
+        font-size: 4.0rem;
+        font-weight: 300;
+    }
+</style>
+<h1>Test</h1>`;
+    }
+
+}
+
+customElements.define('site-banner', SiteBanner);
