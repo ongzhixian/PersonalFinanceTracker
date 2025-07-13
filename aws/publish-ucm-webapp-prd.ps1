eb898c584d31cfe15cb66f2ad9e6b839ab2cbff4
ongzhixian <zhixian@hotmail.com>
2025-07-13 08:31:17 +0800

    test with params

--- aws/publish-ucm-webapp-prd.ps1
+++ aws/publish-ucm-webapp-prd.ps1
@@ -1,4 +1,9 @@
+param(
+    [Parameter(Mandatory=$false)]
+    [String]$targetEnvironment="ucm-webapp-prd"
+)
+
 Write-Host Publish C:\src\github.com\ongzhixian\pft-aws\aws\s3
 aws s3 ls
-Write-Host Deploying %TARGET_ENVIRONMENT%
+Write-Host Deploying $targetEnvironment
 Write-Host Deployment complete.
